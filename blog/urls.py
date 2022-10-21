f43e8e90e60fedad7628b7bba4fee6086fbe1d69
1024_YottaByte <kirill1999z@gmail.com>
2022-10-21 21:02:15 +0300

    added: transitions, transforms to css. renamed: addpost to add_post

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,7 @@ from .views import BlogListView, ColPostsListView, PostView, \
 urlpatterns = [
     path('', ColPostsListView.as_view(), name='home'),
     path('blog/', BlogListView.as_view(), name='blog_list'),
-    path('blog/addpost/', AddPostView.as_view(), name='addpost'),
+    path('blog/addpost/', AddPostView.as_view(), name='add_post'),
     path('blog/<slug:post_slug>/', PostView.as_view(), name='post'),
     path('blog/<slug:post_slug>/update/', UpdatePostView.as_view(), name='update_post'),
     path('register/', RegisterUserView.as_view(), name='register'),
